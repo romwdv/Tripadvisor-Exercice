322a2bcae796192f32529d400b7d522a36d7c684
Romain <romain@MBProm.local>
2026-05-22 17:38:36 +0200

    ajout du script js pour la modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
   const connectModal = document.querySelector("#connect");
   const modal = document.querySelector("#modal");
+  const closeModal = document.querySelector("#close-modal");
 
   connectModal.addEventListener("click", (e) => {
     modal.classList.remove("hide-modal");
+  });
+
+  closeModal.addEventListener("click", (e) => {
+    modal.classList.add("hide-modal");
   });
 });
